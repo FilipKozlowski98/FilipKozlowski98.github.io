53ffd46c374c9ac3341773b4066d10bb13f14209
Filip Kozłowski <f.kozlowski98@gmail.com>
2021-10-20 20:15:43 +0200

    installed packages

--- new/src/App.js
+++ new/src/App.js
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function App() {
-  return "siema";
+  return <MainWrapper>Work in progress, sorry !</MainWrapper>;
 }
 
 export default App;
